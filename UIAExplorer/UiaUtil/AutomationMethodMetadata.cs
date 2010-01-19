77678981d269f0b2315d28b7363fad153799fe51
matt <matt@linux-matt-nb.site>
2010-01-19 23:55:33 +0800

    Complete AutomationPattern methods metadata

--- UIAExplorer/UiaUtil/AutomationMethodMetadata.cs
+++ UIAExplorer/UiaUtil/AutomationMethodMetadata.cs
@@ -25,13 +25,102 @@ namespace Mono.Accessibility.UIAExplorer.UiaUtil
 					typeof(ExpandCollapsePattern).GetMethod("Expand"),
 					typeof(ExpandCollapsePattern).GetMethod("Collapse")
 				});
-			
-			// TODO more patterns... 
-			
+
+			//GridItemPattern, no method
+
+			//GridPattern
+			patternMethods.Add (GridPattern.Pattern,
+				new MethodInfo [] {
+					typeof(GridPattern).GetMethod("GetItem")
+				});
+
 			//InvokePattern
 			patternMethods.Add (InvokePattern.Pattern,
 				new MethodInfo [] {
 					typeof(InvokePattern).GetMethod("Invoke")
+				});
+
+			//MultipleViewPattern
+			patternMethods.Add (MultipleViewPattern.Pattern,
+				new MethodInfo [] {
+					typeof(MultipleViewPattern).GetMethod("GetViewName"),
+					typeof(MultipleViewPattern).GetMethod("SetCurrentView")
+				});
+
+			//RangeValuePattern
+			patternMethods.Add (RangeValuePattern.Pattern,
+				new MethodInfo [] {
+					typeof(RangeValuePattern).GetMethod("SetValue")
+				});
+
+			//ScrollItemPattern
+			patternMethods.Add (ScrollItemPattern.Pattern,
+				new MethodInfo [] {
+					typeof(ScrollItemPattern).GetMethod("ScrollIntoView")
+				});
+			
+			//ScrollPattern
+			patternMethods.Add (ScrollPattern.Pattern,
+				new MethodInfo [] {
+					typeof(ScrollPattern).GetMethod("Scroll"),
+					typeof(ScrollPattern).GetMethod("ScrollHorizontal"),
+					typeof(ScrollPattern).GetMethod("ScrollVertical"),
+					typeof(ScrollPattern).GetMethod("SetScrollPercent"),
+				});
+
+			//SelectionItemPattern
+			patternMethods.Add (SelectionItemPattern.Pattern,
+				new MethodInfo [] {
+					typeof(SelectionItemPattern).GetMethod("AddToSelection"),
+					typeof(SelectionItemPattern).GetMethod("RemoveFromSelection"),
+					typeof(SelectionItemPattern).GetMethod("Select")
+				});
+			
+			//SelectionPattern, no method
+
+			//TableItemPattern, no method
+			
+			//TablePattern, no method
+			patternMethods.Add (TablePattern.Pattern,
+				new MethodInfo [] {
+					typeof(TablePattern).GetMethod("GetItem")
+				});
+
+			//TextPattern
+			patternMethods.Add (TextPattern.Pattern,
+				new MethodInfo [] {
+					typeof(TextPattern).GetMethod("GetSelection"),
+					typeof(TextPattern).GetMethod("GetVisibleRanges"),
+					typeof(TextPattern).GetMethod("RangeFromChild"),
+					typeof(TextPattern).GetMethod("RangeFromPoint")
+				});
+
+			//TogglePattern
+			patternMethods.Add (TogglePattern.Pattern,
+				new MethodInfo [] {
+					typeof(TogglePattern).GetMethod("Toggle")
+				});
+
+			//TransformPattern
+			patternMethods.Add (TransformPattern.Pattern,
+				new MethodInfo [] {
+					typeof(TransformPattern).GetMethod("Move"),
+					typeof(TransformPattern).GetMethod("Resize"),
+					typeof(TransformPattern).GetMethod("Rotate"),
+				});
+
+			//ValuePattern
+			patternMethods.Add (ValuePattern.Pattern,
+				new MethodInfo [] {
+					typeof(ValuePattern).GetMethod("SetValue")
+				});
+
+			//WindowPattern
+			patternMethods.Add (WindowPattern.Pattern,
+				new MethodInfo [] {
+					typeof(WindowPattern).GetMethod("Close"),
+					typeof(WindowPattern).GetMethod("SetWindowVisualState"),
+					typeof(WindowPattern).GetMethod("WaitForInputIdle")
 				});
 		}
 
