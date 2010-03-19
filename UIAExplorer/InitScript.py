53931d1927645748caff8b22087c5a53f55b8d5a
matt <matt@linux-matt-nb.site>
2010-03-19 16:51:16 +0800

    add missing file

---
+++ UIAExplorer/InitScript.py
@@ -0,0 +1,13 @@
+from System import *
+from System.Collections.Generic import *
+from System.Windows import *
+from System.Windows.Automation import *
+from System.Linq import *
+
+def help() :
+	print """Use the variable named "acc" to access selected AutomationElement.
+Use PageUp/PageDown to navigate command history.
+Press Ctrl+J to open the auto-completion window.
+Press Escape to close the auto-completion window.
+
+Bug reports -> matt@mattguo.com"""
